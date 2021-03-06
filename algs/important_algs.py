7c62f6e3ef642b48b9f918fb6ca6e93ece2d771a
Jakub Lemiesiewicz <jakub.lemiesiewicz@gmail.com>
2021-03-06 23:25:46 +0100

    init commit

---
+++ algs/important_algs.py
@@ -0,0 +1,14 @@
+def alg_min(a, b):
+    # Funkcja zwraca mniejszą liczbę
+    if a < b:
+        return a
+    return b
+
+def alg_max(a, b):
+    # Funkcjia zwraca największą liczbę 
+    if a > b:
+        return a
+    return b
+
+def nwd(a, b):
+    return None
---
+++ tests.py
@@ -0,0 +1,16 @@
+from algs import important_algs as ia
+
+
+class Test:
+    def test_test(self):
+        assert True == True
+        assert False == False
+
+
+class TestALGS:
+    def test_alg_min(self):
+        assert ia.alg_min(5, 10) == 5
+
+    def test_alg_max(self):
+
+
