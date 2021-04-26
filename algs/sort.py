85e3b953b43801c3cbc46d017951919de3b743c1
Jakub Lemiesiewicz <jakub.lemiesiewicz@gmail.com>
2021-04-26 19:08:37 +0200

    heap sort

--- algs/sort.py
+++ algs/sort.py
@@ -52,3 +52,25 @@ def bubble_sort_v4(array):
                 array[j] = array[j+1]
                 array[j+1] = temp
     return array
+
+
+def heap_sort(array):
+    def heap_sort_func(array, n, i):
+        largest = i
+        left = 2*i+1
+        right = 2*i+2
+        if left < n and array[left] > array[largest]:
+            largest = left
+        if right < n and array[right] > array[largest]:
+            largest = right
+        if largest != i:
+            array[i], array[largest] = array[largest], array[i]
+            heap_sort_func(array, n, largest)
+    
+    for i in range(int(round(len(array)/2)), -1, -1):
+        heap_sort_func(array, len(array), i)
+    for i in range(len(array)-1, -1, -1):
+        array[i], array[0] = array[0], array[i]
+        heap_sort_func(array, i, 0)
+
+    return array
